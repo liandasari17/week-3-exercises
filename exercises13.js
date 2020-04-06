cd977bc2d4d75d086718532ebd9d5c4d9bad4f41
liandasari17 <62257074+liandasari17@users.noreply.github.com>
2020-04-06 18:10:47 +0700

    exercises 13

--- exercises13.js
+++ exercises13.js
@@ -1,24 +1,35 @@
 function targetTerdekat(arr) {
-    if(arr.indexOf('x') ===  -1) return 0
-    
-    var posisiO = arr.indexOf('o')
-    var temp = arr.length
-    //console.log(temp)
-    for(i = 0; i < arr.length; i++){
-      if(arr[i] === 'x'){
-        var jarak = Math.abs(i-posisiO);
-        if(jarak < temp){
-          temp = jarak
+    var o = 0;
+    var x = [];
+    var temp = 0;
+
+    while(temp < arr.length){
+        if (arr[temp] === 'o'){
+            o = temp;
         }
+
+        if (arr[temp] === 'x'){
+            x.push(temp);
         }
-      }
-    
-    return temp
+        temp++;      
+    }
+    xFurthest = x[x.length-1]
+
+    for (i = 0; i < x.length; i++){
+        
+        if (o < x[i]){
+            return x[i] - o;
+        }else if (o > xFurthest){
+            return o - xFurthest;
+        } 
+    }
+    return 0;
   }
   
-  // TEST CASES
-  console.log(targetTerdekat(['', '', 'o', '', '', 'x', '', 'x'])); // 3
-  console.log(targetTerdekat(['o', '', '', '', 'x', 'x', 'x'])); // 4
-  console.log(targetTerdekat(['x', '', '', '', 'x', 'x', 'o', ''])); // 1
-  console.log(targetTerdekat(['', '', 'o', ''])); // 0
-  console.log(targetTerdekat(['', 'o', '', 'x', 'x', '', '', 'x'])); // 2
+// TEST CASES
+console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])); // 3
+console.log(targetTerdekat(['o', ' ', ' ', ' ', 'x', 'x', 'x'])); // 4
+console.log(targetTerdekat(['x', ' ', ' ', ' ', 'x', 'x', 'o', ' '])); // 1
+console.log(targetTerdekat([' ', ' ', 'o', ' '])); // 0
+console.log(targetTerdekat([' ', 'o', ' ', 'x', 'x', ' ', ' ', 'x'])); // 2
+console.log(targetTerdekat(['x', 'x', 'x', ' ', 'o'])); //2
