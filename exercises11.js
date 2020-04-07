f33cdcc2bd4b6dc0312a776a8287e30fbd9c5639
liandasari17 <62257074+liandasari17@users.noreply.github.com>
2020-04-07 16:18:19 +0700

    exercises 11 12

--- exercises11.js
+++ exercises11.js
@@ -1,18 +1,22 @@
 function tentukanDeretAritmatika(arr) {
-    var status;
-    for(i=0; i < arr.length-2; i++){
-      if((arr[i+1]- arr[i]) === (arr[i+2]-arr[i+1])){
-        status = true
-      } else {
-        status = false
-      }
-      }
-      return status
-    }
+  // you can only write your code here!
+  var cek = false
+  var nilai = arr[1] - arr[0]
+
+  for(i = 0; i < arr.length-1; i++){
   
-  // TEST CASES
-  console.log(tentukanDeretAritmatika([1, 2, 3, 4, 5, 6])); // true
-  console.log(tentukanDeretAritmatika([2, 4, 6, 12, 24])); // false
-  console.log(tentukanDeretAritmatika([2, 4, 6, 8])); // true
-  console.log(tentukanDeretAritmatika([2, 6, 18, 54])); // false
-  console.log(tentukanDeretAritmatika([1, 2, 3, 4, 7, 9])); // false
+      if(arr[i+1] === arr[i] + nilai){
+          cek = true
+      }
+      else{
+          return false
+      }
+  }
+  return cek
+  }
+// TEST CASES
+console.log(tentukanDeretAritmatika([1, 2, 3, 4, 5, 6])); // true
+console.log(tentukanDeretAritmatika([2, 4, 6, 12, 24])); // false
+console.log(tentukanDeretAritmatika([2, 4, 6, 8])); // true
+console.log(tentukanDeretAritmatika([2, 6, 18, 54])); // false
+console.log(tentukanDeretAritmatika([1, 2, 3, 4, 7, 9])); // false
--- exercises12.js
+++ exercises12.js
@@ -1,18 +1,24 @@
 function tentukanDeretGeometri(arr) {
-    var status;
-    for(i=0; i<arr.length-2;i++){
-      if((arr[i+1] / arr[i]) === (arr[i+2]/ arr[i+1])){
-        status= true
-      } else {
-        status= false
-      }
-    }
-    return status
-  }
+  // you can only write your code here!
+  var cek = false
+  var nilai = arr[1] / arr[0]
+
+  for(i = 0; i < arr.length-1; i++){
   
-  // TEST CASES
-  console.log(tentukanDeretGeometri([1, 3, 9, 27, 81])); // true
-  console.log(tentukanDeretGeometri([2, 4, 8, 16, 32])); // true
-  console.log(tentukanDeretGeometri([2, 4, 6, 8])); // false
-  console.log(tentukanDeretGeometri([2, 6, 18, 54])); // true
-  console.log(tentukanDeretGeometri([1, 2, 3, 4, 7, 9])); // false
+      if(arr[i+1] === arr[i] * nilai){
+          cek = true
+      }
+      else{
+          return false
+      }
+  }
+  return cek
+
+}
+
+// TEST CASES
+console.log(tentukanDeretGeometri([1, 3, 9, 27, 81])); // true
+console.log(tentukanDeretGeometri([2, 4, 8, 16, 32])); // true
+console.log(tentukanDeretGeometri([2, 4, 6, 8])); // false
+console.log(tentukanDeretGeometri([2, 6, 18, 54])); // true
+console.log(tentukanDeretGeometri([1, 2, 3, 4, 7, 9])); // false
